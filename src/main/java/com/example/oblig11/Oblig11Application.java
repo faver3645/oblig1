cb1955815d633c9e93740dab101e82a376844c48
faver3645 <143396926+faver3645@users.noreply.github.com>
2024-02-09 13:10:21 +0100

    første commit

---
+++ src/main/java/com/example/oblig11/Oblig11Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig11/Oblig11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
